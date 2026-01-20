3780e8a20113aed448f051d5b5a0649e2c79dce9
Aarik Maharjan <mhrznaarik870@gmail.com>
2026-01-20 21:16:02 +0545

    Remove Sanctum configuration file, add role column to users table, and update API routes for authentication

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use GuzzleHttp\Promise\Create;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|min:5',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+            'phoneno' => 'required|digits:10|regex:/^98[0-9]{8}$/',
+            'image_url' => 'nullable|mimes:jpeg,png,jpg,gif,webp|max:20480',
+            'role' => 'in:admin,user',
+        ]);
+        try {
+            $response = DB::transaction(function () use ($request) {
+                if ($request->role === 'admin' && Auth::user() != 'admin') {
+                    return response()->json([
+                        'status' => 0,
+                        'message' => 'Unauthorized to create admin account!',
+                    ]);
+                }
+                User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => $request->password,
+                    'phoneno' => $request->phoneno,
+                    'image_url' => $request->image_url,
+                    'role' => $request->role,
+                    'created_at' => now()
+                ]);
+
+                return response()->json([
+                    'status' => 1,
+                    'message' => 'Account created sucessfully'
+                ], 200);
+            });
+
+            return $response;
+
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        try {
+            $credentials = $request->only('email', 'password');
+            $user = User::where('email', $credentials['email'])->first();
+
+            if (!$user) {
+                return response()->json([
+                    'status' => 0,
+                    'message' => "No user found for '$credentials[email]'. Please register first!",
+                ], 404);
+            }
+
+            foreach ($user->tokens as $token) {
+                $token->revoke();
+            }
+
+            if (!Auth::attempt($credentials)) {
+                return response()->json([
+                    'status' => 0,
+                    'message' => 'Invalid credentials!'
+                ], 401);
+            }
+
+            $accessToken = $user->createToken('authToken')->accessToken;
+
+            return response()->json([
+                'message' => 'Login Successful!',
+                'data' => [
+                    'status' => 1,
+                    'user' => Auth::user(),
+                    'access_token' => "Bearer-$accessToken",
+                ]
+            ]);
+
+        } catch (\Exception $e) {
+            Log::error('Login error' . $e->getMessage());
+            return response()->json([
+                'status' => '0',
+                'message' => 'Login failed!'
+            ]);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'status' => 1,
+            'message' => 'Logged out sucessfully!'
+        ]);
+
+    }
+}
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,7 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -22,6 +22,13 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phoneno',
+        'role',
+        'deleted_at',
+        'email_verified_at',
+        'remember_token',
+        'created_at',
+        'updated_at',
     ];
 
     /**
@@ -32,6 +39,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'created_at',
+        'updated_at'
     ];
 
     /**
@@ -44,6 +53,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
         ];
     }
 }
